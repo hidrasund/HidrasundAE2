9c6244f68462afe5957790dcb2d55fc241032d6d
Alex Robinson <alerobo@hotmail.co.uk>
2019-03-25 19:42:46 +0000

    Completed UI
    UI including a pause menu for the game that will take the user back to the main menu

---
+++ UI for swegmt/Assets/HealthAndSanity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthAndSanity : MonoBehaviour {
+
+    private Text MyTXT;
+    public int maxHP = 10;
+    public int CurrentHP = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyTXT = GameObject.Find("Text1").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MyTXT.text = CurrentHP.ToString();
+    }
+}
