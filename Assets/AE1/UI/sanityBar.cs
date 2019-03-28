5a279b1b953fd5ae96384c9af7d8ab36882eb9cf
Alex Robinson <alerobo@hotmail.co.uk>
2019-03-28 23:41:24 +0000

    UI implemented
    added the UI so now health and sanity is displayed

---
+++ Assets/AE1/UI/sanityBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class sanityBar : MonoBehaviour
+{
+    private Text MyTXT;
+    public int maxSP = 10;
+    public int CurrentSP = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyTXT = GameObject.Find("Text2").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MyTXT.text = CurrentSP.ToString();
+    }
+}
