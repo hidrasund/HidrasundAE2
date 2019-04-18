1496e1ec44a86afb8346b9102a40e951cd2fa831
Alex Robinson <alerobo@hotmail.co.uk>
2019-04-18 01:11:31 +0100

    inventory
    an inventory has been added to the player

---
+++ Assets/AE1/UI/Inventory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public Item[] itemList = new Item [4];
+    public List<InventorySlot> inventorySlots = new List<InventorySlot>();
+
+    public bool AddItem(Item item)
+    {
+        for (int i =0; i < itemList.Length; i++)
+        {
+            if (itemList[i] == null)
+            {
+
+                itemList[i] = item;
+                return true;
+
+            }
+        }
+        return false;
+    }
+
+
+}
---
+++ Assets/AE1/UI/Item.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Item", menuName ="Inventory/Item")]
+public class Item : ScriptableObject
+{
+    public string itemName = "New Item";
+    public enum Type { Ouja, Bell, Dynamite, CrystalBall }
+    public Type type = Type.Ouja;
+}
